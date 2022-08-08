5b5dc3a922513522f5fcc2d782cdb46a6057f71e
Aatish sharma <73808977+Aatish1066@users.noreply.github.com>
2022-08-08 18:53:47 +0530

    Create simpleProgram.cpp

---
+++ simpleProgram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+    // basic data types in c++
+    int a=4;
+    bool b=true;
+    char c='d';
+    float d=4.00;
+    double e=8.00;
+    cout<<"Size of char :"<<sizeof(char)<<endl;
+    cout<<"Size of int :"<<sizeof(int)<<endl;
+    cout<<"Size of bool :"<<sizeof(bool)<<endl;
+    cout<<"Size of float :"<<sizeof(float)<<endl;
+    cout<<"Size of double :"<<sizeof(double)<<endl;
+    getch();
+}
